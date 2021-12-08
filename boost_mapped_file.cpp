d1efa6a4b32abf6df852624c14c46c9008d141d7
montura <fatum.est.series.causarum@gmail.com>
2021-12-08 18:40:06 +0300

    Look at flat_map example

--- boost_mapped_file.cpp
+++ boost_mapped_file.cpp
@@ -4,13 +4,59 @@ namespace fs = std::filesystem;
 
 #include <boost/iostreams/device/mapped_file.hpp>
 #include <boost/iostreams/stream.hpp>
-namespace bio = boost::iostreams;
+#include <boost/fusion/adapted/std_pair.hpp>
+#include <boost/container/flat_map.hpp>
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <boost/spirit/include/qi.hpp>
+
+namespace m_boost {
+    namespace bio = boost::iostreams;
+    namespace bip = boost::interprocess;
+    namespace bc = boost::container;
+    namespace bqi = boost::spirit::qi;
+
+    template <typename T>
+    using allocator = bip::allocator<T, bip::managed_mapped_file::segment_manager>;
+
+    template <typename K, typename V>
+    using map = bc::flat_map<
+        K, V, std::less<K>,
+        allocator<typename bc::flat_map<K, V>::value_type> >;
+
+    template <typename K, typename V>
+    using Map = map<K, V>;
+}
+
+using namespace m_boost;
+
+// todo: investigate
+//  https://stackoverflow.com/questions/28217301/using-boostiostreamsmapped-file-source-with-stdmultimap
+void test_flat_map(const char* fname) {
+    bip::managed_mapped_file msm(bip::open_or_create, "lookup.bin", 10ul << 20);
+    Map<int, int>* lookup = msm.find_or_construct<Map<int, int>>("lookup")(msm.get_segment_manager());
+
+    if (lookup->empty()) {
+        // only read input file if not already loaded
+        bio::mapped_file_source input(fname);
+        auto f(input.data()), l(f + input.size());
+
+        bool ok = bqi::phrase_parse(f, l,
+                                   (bqi::auto_ >> bqi::auto_) % bqi::eol >> *bqi::eol,
+                                    bqi::blank, *lookup);
+
+        if (!ok || (f!=l))
+            throw std::runtime_error("Error during parsing at position #" + std::to_string(f - input.data()));
+    }
+}
 
 int main() {
    std::string_view in = "../mmap_in.data";
    std::string_view out = "../mmap_out.data";
 
-    boost::iostreams::mapped_file_source file;
+    // mapped_file_source is read only
+    // mapped_file_sink is write only
+    // mapped_file is both read and write
+    bio::mapped_file_source file;
     file.open(in.data());
 
     if (file.is_open()) {
