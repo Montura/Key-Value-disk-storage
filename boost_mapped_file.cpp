51cda3728f858d6c03122f56efac523f91760dd2
montura <fatum.est.series.causarum@gmail.com>
2021-12-13 14:58:49 +0300

    Working on mapping 4Kb regions of 1MB file and write them to the output file

--- boost_mapped_file.cpp
+++ boost_mapped_file.cpp
@@ -41,40 +41,62 @@ void test_flat_map(const char* fname) {
         auto f(input.data()), l(f + input.size());
 
         bool ok = bqi::phrase_parse(f, l,
-                                   (bqi::auto_ >> bqi::auto_) % bqi::eol >> *bqi::eol,
+                                    (bqi::auto_ >> bqi::auto_) % bqi::eol >> *bqi::eol,
                                     bqi::blank, *lookup);
 
-        if (!ok || (f!=l))
+        if (!ok || (f != l))
             throw std::runtime_error("Error during parsing at position #" + std::to_string(f - input.data()));
     }
 }
 
 int main() {
-   std::string_view in = "../mmap_in.data";
-   std::string_view out = "../mmap_out.data";
+    constexpr char END_OF_LINE = '\n';
+    constexpr std::size_t kB = 1024;
+    constexpr std::size_t page_size = 4 * kB;
+    constexpr std::size_t MB = 1024 * kB;
+//    constexpr std::size_t GB = 1024 * MB;
+
+    static_assert(sizeof(boost::uintmax_t) == sizeof(long long));
+    assert(page_size == bip::mapped_region::get_page_size());
+
+    std::string_view in = "../mmap_in.data";   // 1 MB
+    std::string_view out = "../mmap_out.data"; // 1 MB
 
     // mapped_file_source is read only
     // mapped_file_sink is write only
     // mapped_file is both read and write
-    bio::mapped_file_source file;
-    file.open(in.data());
+//    bio::mapped_file_params in_params;
+//    in_params.path = in.data();
+//    in_params.flags = bio::mapped_file::mapmode::readonly;
 
-    if (file.is_open()) {
-        static_assert(sizeof(boost::uintmax_t) == sizeof(long long));
-        auto fileSize = static_cast<long long>(fs::file_size(fs::path(in.data())));
+    constexpr std::size_t mapped_regions = MB / page_size;
+    std::vector<bio::mapped_file_source> mmapRegions(mapped_regions);
+    std::for_each(
+        mmapRegions.begin(),
+        mmapRegions.end(),
+        [&in, offset = 0](bio::mapped_file_source& file) mutable {
+            file.open(in.data(), page_size, offset);
+            offset += page_size;
+        }
+    );
 
-        bio::mapped_file_params out_params;
-        out_params.path = out.data();
-        out_params.new_file_size = fileSize;
-        out_params.flags = bio::mapped_file::mapmode::readwrite;
+    bio::mapped_file_params out_params;
+    out_params.path = out.data();
+    out_params.new_file_size = MB;
+    out_params.flags = bio::mapped_file::mapmode::readwrite;
 
-        bio::stream<bio::mapped_file_sink> out_mapped(out_params);
-
-        std::copy(file.data(), file.data() + fileSize, std::ostream_iterator<char>(out_mapped));
-        out_mapped.close();
-    }
-    file.close();
-
+    bio::stream<bio::mapped_file_sink> out_mapped(out_params);
+    std::for_each(
+        mmapRegions.begin(),
+        mmapRegions.end(),
+        [&out_mapped](bio::mapped_file_source& file) {
+            std::transform(file.data(), file.end(), std::ostream_iterator<char>(out_mapped),
+                [](char byte) -> char { return (byte != END_OF_LINE) ? static_cast<char>(byte + 1) : byte; }
+            );
+            file.close();
+        }
+    );
+    out_mapped.close();
 
     return 0;
 }
