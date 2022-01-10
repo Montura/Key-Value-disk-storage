f039bd7a0943d6e2a5f0e31ec76cfbeffbf10cd5
montura <fatum.est.series.causarum@gmail.com>
2022-01-10 14:50:07 +0300

    Add KEY template arg for test functions

--- test/storage_tests.h
+++ test/storage_tests.h
@@ -15,18 +15,17 @@ namespace storage_tests {
     template <typename VolumeT, typename K, typename V>
     void set(VolumeT& volume, const K& key, const V& val) {
         if constexpr(std::is_pointer_v<V>) {
-            auto size =  static_cast<int32_t>(std::strlen(val));
-            volume.set(key, val, size);
+            volume.set(key, val, SizeInfo<K,V>::value_size_in_bytes(val));
         } else {
             volume.set(key, val);
         }
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_test_emtpy_file(std::string const& name, int const order) {
         std::string db_name = "../" + name + ".txt";
         {
-            Storage<int32_t, V> s;
+            Storage<K, V> s;
             s.open_volume(db_name, order);
         }
         bool success = fs::file_size(db_name) == 0;
@@ -34,17 +33,17 @@ namespace storage_tests {
         return success;
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_test_file_size_with_one_entry(std::string const& name, int const order) {
         std::string db_name = "../" + name + ".txt";
 
-        Storage<int32_t, V> s;
-        const int32_t key = 0;
+        Storage<K, V> s;
+        const K key = 0;
         ValueGenerator<V> g;
         const V val = g.next_value(key);
 
         auto on_exit = [&](const auto& volume, bool after_remove = false) -> bool {
-            uint32_t total_size = SizeInfo<int32_t,V>::file_size_in_bytes(order, key, val, after_remove);
+            uint32_t total_size = SizeInfo<K,V>::file_size_in_bytes(order, key, val, after_remove);
             s.close_volume(volume);
             return (fs::file_size(db_name) == total_size);
         };
@@ -70,14 +69,14 @@ namespace storage_tests {
         return success;
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_test_set_get_one(std::string const& name, int const order) {
         std::string db_name = "../" + name + ".txt";
-        Storage<int32_t, V> s;
+        Storage<K, V> s;
 
-        int32_t key = 0;
+        const K key = 0;
         ValueGenerator<V> g;
-        V expected_val = g.next_value(key);
+        const V expected_val = g.next_value(key);
         bool success = false;
         {
             auto volume = s.open_volume(db_name, order);
@@ -97,15 +96,15 @@ namespace storage_tests {
         return success;
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_test_remove_one(std::string const& name, int const order) {
         std::string db_name = "../" + name + ".txt";
-        Storage<int32_t, V> s;
+        Storage<K, V> s;
 
-        int32_t key = 0;
+        const K key = 0;
         ValueGenerator<V> g;
-        V expected_val = g.next_value(key);
-        uint32_t total_size = SizeInfo<int32_t, V>::header_size_in_bytes();
+        const V expected_val = g.next_value(key);
+        uint32_t total_size = SizeInfo<K, V>::header_size_in_bytes();
 
         bool success = false;
         {
@@ -127,15 +126,15 @@ namespace storage_tests {
         return success;
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_test_repeatable_operations_on_a_unique_key(std::string const& name, int const order) {
         std::string db_name = "../" + name + ".txt";
-        Storage<int32_t, V> s;
+        Storage<K, V> s;
 
-        int32_t key = 0;
+        const K key = 0;
         ValueGenerator<V> g;
-        V expected_val = g.next_value(key);
-        uint32_t header_size = SizeInfo<int32_t,V>::header_size_in_bytes();
+        const V expected_val = g.next_value(key);
+        uint32_t header_size = SizeInfo<K,V>::header_size_in_bytes();
 
         auto volume = s.open_volume(db_name, order);
         bool success = true;
@@ -152,14 +151,14 @@ namespace storage_tests {
         return success;
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_test_set_on_the_same_key(std::string const& name, int const order) {
         std::string db_name = "../" + name + ".txt";
-        Storage<int32_t, V> s;
+        Storage<K, V> s;
 
         auto volume = s.open_volume(db_name, order);
         bool success = true;
-        int32_t key = 0;
+        const K key = 0;
 
         ValueGenerator<V> g;
         for (int i = 0; i < 1000; ++i) {
@@ -174,7 +173,7 @@ namespace storage_tests {
         return success;
     }
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_on_random_values(std::string const& name, int const order, int const n) {
         std::string db_name = "../" + name + ".txt";
         int rounds = 3;
@@ -184,19 +183,19 @@ namespace storage_tests {
         bool success = true;
         for (int i = 0; i < rounds; ++i) {
             auto keys_to_remove = generate_rand_keys();
-            success &= TestRunner<int32_t, V>::run(db_name, order, n, keys_to_remove);
+            success &= TestRunner<K, V>::run(db_name, order, n, keys_to_remove);
         }
         fs::remove(db_name);
         return success;
     };
 
-    template <typename V>
+    template <typename K, typename V>
     bool run_multithreading_test(ThreadPool& pool, std::string const& name, int const order, int const n) {
         std::string db_name = "../" + name + ".txt";
 #ifdef DEBUG
         std::cout << "Run multithreading test on " << n << " elements on 10 threads: " << std::endl;
 #endif
-        bool success = TestRunnerMT<int32_t, V>::run(pool, db_name, order, n);
+        bool success = TestRunnerMT<K, V>::run(pool, db_name, order, n);
         fs::remove(db_name);
         return success;
     };
--- test/test.cpp
+++ test/test.cpp
@@ -10,86 +10,86 @@ namespace tests {
 
     BOOST_AUTO_TEST_CASE(test_empty_file) {
         int order = 2;
-        bool success = run_test_emtpy_file<int64_t>("empty_s_i32", order);
-        success &= run_test_emtpy_file<int64_t>("empty_s_i64", order);
-        success &= run_test_emtpy_file<float>("empty_s_f", order);
-        success &= run_test_emtpy_file<double>("empty_s_d", order);
-        success &= run_test_emtpy_file<std::string>("empty_s_str", order);
-        success &= run_test_emtpy_file<std::wstring>("empty_s_wstr", order);
-        success &= run_test_emtpy_file<const char*>("empty_s_blob", order);
+        bool success = run_test_emtpy_file<int32_t, int64_t>("empty_s_i32", order);
+        success &= run_test_emtpy_file<int32_t, int64_t>("empty_s_i64", order);
+        success &= run_test_emtpy_file<int32_t, float>("empty_s_f", order);
+        success &= run_test_emtpy_file<int32_t, double>("empty_s_d", order);
+        success &= run_test_emtpy_file<int32_t, std::string>("empty_s_str", order);
+        success &= run_test_emtpy_file<int32_t, std::wstring>("empty_s_wstr", order);
+        success &= run_test_emtpy_file<int32_t, const char*>("empty_s_blob", order);
         BOOST_REQUIRE_MESSAGE(success, "TEST_EMPTY_FILE");
     }
 
     BOOST_AUTO_TEST_CASE(file_size_after_set_one_element) {
         int order = 2;
-        bool success = run_test_file_size_with_one_entry<int32_t>("one_s_i32", order);
-        success &= run_test_file_size_with_one_entry<int64_t>("one_s_i64", order);
-        success &= run_test_file_size_with_one_entry<float>("one_s_f", order);
-        success &= run_test_set_get_one<double>("one_s_d", order);
-        success &= run_test_file_size_with_one_entry<std::string>("one_s_str", order);
-        success &= run_test_file_size_with_one_entry<std::wstring>("one_s_wstr", order);
-        success &= run_test_file_size_with_one_entry<const char*>("one_s_blob", order);
+        bool success = run_test_file_size_with_one_entry<int32_t, int32_t>("one_s_i32", order);
+        success &= run_test_file_size_with_one_entry<int32_t, int64_t>("one_s_i64", order);
+        success &= run_test_file_size_with_one_entry<int32_t, float>("one_s_f", order);
+        success &= run_test_set_get_one<int32_t, double>("one_s_d", order);
+        success &= run_test_file_size_with_one_entry<int32_t, std::string>("one_s_str", order);
+        success &= run_test_file_size_with_one_entry<int32_t, std::wstring>("one_s_wstr", order);
+        success &= run_test_file_size_with_one_entry<int32_t, const char*>("one_s_blob", order);
         BOOST_REQUIRE_MESSAGE(success, "TEST_FILE_SIZE");
     }
 
     BOOST_AUTO_TEST_CASE(set_get_one_element) {
         int order = 2;
-        bool success = run_test_set_get_one<int32_t>("get_one_s_i32", order);
-        success &= run_test_set_get_one<int64_t>("get_one_s_i64", order);
-        success &= run_test_set_get_one<float>("get_one_s_f", order);
-        success &= run_test_set_get_one<double>("get_one_s_d", order);
-        success &= run_test_set_get_one<std::string>("get_one_s_str", order);
-        success &= run_test_set_get_one<std::wstring>("get_one_s_wstr", order);
-        success &= run_test_set_get_one<const char*>("get_one_s_blob", order);
+        bool success = run_test_set_get_one<int32_t, int32_t>("get_one_s_i32", order);
+        success &= run_test_set_get_one<int32_t, int64_t>("get_one_s_i64", order);
+        success &= run_test_set_get_one<int32_t, float>("get_one_s_f", order);
+        success &= run_test_set_get_one<int32_t, double>("get_one_s_d", order);
+        success &= run_test_set_get_one<int32_t, std::string>("get_one_s_str", order);
+        success &= run_test_set_get_one<int32_t, std::wstring>("get_one_s_wstr", order);
+        success &= run_test_set_get_one<int32_t, const char*>("get_one_s_blob", order);
         BOOST_REQUIRE_MESSAGE(success, "TEST_SET_GET_ONE_ELEMENT");
     }
 
     BOOST_AUTO_TEST_CASE(remove_one_element) {
         int order = 2;
-        bool success = run_test_remove_one<int32_t>("remove_one_s_i32", order);
-        success &= run_test_remove_one<int64_t>("remove_one_s_i64", order);
-        success &= run_test_remove_one<float>("remove_one_s_f", order);
-        success &= run_test_remove_one<double>("remove_one_s_d", order);
-        success &= run_test_remove_one<std::string>("remove_one_s_str", order);
-        success &= run_test_remove_one<std::wstring>("remove_one_s_wstr", order);
-        success &= run_test_remove_one<const char*>("remove_one_s_blob", order);
+        bool success = run_test_remove_one<int32_t, int32_t>("remove_one_s_i32", order);
+        success &= run_test_remove_one<int32_t, int64_t>("remove_one_s_i64", order);
+        success &= run_test_remove_one<int32_t, float>("remove_one_s_f", order);
+        success &= run_test_remove_one<int32_t, double>("remove_one_s_d", order);
+        success &= run_test_remove_one<int32_t, std::string>("remove_one_s_str", order);
+        success &= run_test_remove_one<int32_t, std::wstring>("remove_one_s_wstr", order);
+        success &= run_test_remove_one<int32_t, const char*>("remove_one_s_blob", order);
         BOOST_REQUIRE_MESSAGE(success, "TEST_REMOVE_ONE_ELEMENT");
     }
 
     BOOST_AUTO_TEST_CASE(repeatable_operations_on_a_unique_key) {
         int order = 2;
-        bool success = run_test_repeatable_operations_on_a_unique_key<int32_t>("repeatable_set_s_i32", order);
-        success &= run_test_repeatable_operations_on_a_unique_key<int64_t>("repeatable_set_s_i64", order);
-        success &= run_test_repeatable_operations_on_a_unique_key<float>("repeatable_set_s_f", order);
-        success &= run_test_repeatable_operations_on_a_unique_key<double>("repeatable_set_s_d", order);
-        success &= run_test_repeatable_operations_on_a_unique_key<std::string>("repeatable_set_s_str", order);
-        success &= run_test_repeatable_operations_on_a_unique_key<std::wstring>("repeatable_set_s_wstr", order);
-        success &= run_test_repeatable_operations_on_a_unique_key<const char*>("repeatable_set_s_blob", order);
+        bool success = run_test_repeatable_operations_on_a_unique_key<int32_t, int32_t>("repeatable_set_s_i32", order);
+        success &= run_test_repeatable_operations_on_a_unique_key<int32_t, int64_t>("repeatable_set_s_i64", order);
+        success &= run_test_repeatable_operations_on_a_unique_key<int32_t, float>("repeatable_set_s_f", order);
+        success &= run_test_repeatable_operations_on_a_unique_key<int32_t, double>("repeatable_set_s_d", order);
+        success &= run_test_repeatable_operations_on_a_unique_key<int32_t, std::string>("repeatable_set_s_str", order);
+        success &= run_test_repeatable_operations_on_a_unique_key<int32_t, std::wstring>("repeatable_set_s_wstr", order);
+        success &= run_test_repeatable_operations_on_a_unique_key<int32_t, const char*>("repeatable_set_s_blob", order);
         BOOST_REQUIRE_MESSAGE(success, "TEST_REPEATABLE_OPERATIONS");
     }
 
     BOOST_AUTO_TEST_CASE(set_various_values_on_the_same_key) {
         int order = 2;
-        bool success = run_test_set_on_the_same_key<int32_t>("various_set_s_i32", order);
-        success &= run_test_set_on_the_same_key<int64_t>("various_set_s_i64", order);
-        success &= run_test_set_on_the_same_key<float>("various_set_s_f", order);
-        success &= run_test_set_on_the_same_key<double>("various_set_s_d", order);
-        success &= run_test_set_on_the_same_key<std::string>("various_set_s_str", order);
-        success &= run_test_set_on_the_same_key<std::wstring>("various_set_s_wstr", order);
-        success &= run_test_set_on_the_same_key<const char*>("various_set_s_blob", order);
+        bool success = run_test_set_on_the_same_key<int32_t, int32_t>("various_set_s_i32", order);
+        success &= run_test_set_on_the_same_key<int32_t, int64_t>("various_set_s_i64", order);
+        success &= run_test_set_on_the_same_key<int32_t, float>("various_set_s_f", order);
+        success &= run_test_set_on_the_same_key<int32_t, double>("various_set_s_d", order);
+        success &= run_test_set_on_the_same_key<int32_t, std::string>("various_set_s_str", order);
+        success &= run_test_set_on_the_same_key<int32_t, std::wstring>("various_set_s_wstr", order);
+        success &= run_test_set_on_the_same_key<int32_t, const char*>("various_set_s_blob", order);
         BOOST_REQUIRE_MESSAGE(success, "TEST_SET_VARIOUS_VALUES");
     }
 
     BOOST_AUTO_TEST_CASE(test_on_random_values) {
         int order = 2;
         int elements_count = 10000;
-        bool success = run_on_random_values<int32_t>("random_s_i32", order, elements_count);
-        success &= run_on_random_values<int64_t>("random_s_i64", order, elements_count);
-        success &= run_on_random_values<float>("random_s_f", order, elements_count);
-        success &= run_on_random_values<double>("random_s_d", order, elements_count);
-        success &= run_on_random_values<std::string>("random_s_str", order, elements_count);
-        success &= run_on_random_values<std::wstring>("random_s_wstr", order, elements_count);
-        success &= run_on_random_values<const char*>("random_s_blob", order, elements_count);
+        bool success = run_on_random_values<int32_t, int32_t>("random_s_i32", order, elements_count);
+        success &= run_on_random_values<int32_t, int64_t>("random_s_i64", order, elements_count);
+        success &= run_on_random_values<int32_t, float>("random_s_f", order, elements_count);
+        success &= run_on_random_values<int32_t, double>("random_s_d", order, elements_count);
+        success &= run_on_random_values<int32_t, std::string>("random_s_str", order, elements_count);
+        success &= run_on_random_values<int32_t, std::wstring>("random_s_wstr", order, elements_count);
+        success &= run_on_random_values<int32_t, const char*>("random_s_blob", order, elements_count);
         BOOST_REQUIRE_MESSAGE(success, "TEST_RANDOM_VALUES");
     }
 
@@ -97,13 +97,13 @@ namespace tests {
         int order = 2;
         int elements_count = 10000;
         ThreadPool pool(10);
-        bool success = run_multithreading_test<int32_t>(pool, "mt_s_i32", order, elements_count);
-        success &= run_multithreading_test<int64_t>(pool, "mt_s_i64", order, elements_count);
-        success &= run_multithreading_test<float>(pool, "mt_s_f", order, elements_count);
-        success &= run_multithreading_test<double>(pool, "mt_s_d", order, elements_count);
-        success &= run_multithreading_test<std::string>(pool, "mt_s_str", order, elements_count);
-        success &= run_multithreading_test<std::wstring>(pool, "mt_s_wstr", order, elements_count);
-        success &= run_multithreading_test<const char*>(pool, "mt_s_blob", order, elements_count);
+        bool success = run_multithreading_test<int32_t, int32_t>(pool, "mt_s_i32", order, elements_count);
+        success &= run_multithreading_test<int32_t, int64_t>(pool, "mt_s_i64", order, elements_count);
+        success &= run_multithreading_test<int32_t, float>(pool, "mt_s_f", order, elements_count);
+        success &= run_multithreading_test<int32_t, double>(pool, "mt_s_d", order, elements_count);
+        success &= run_multithreading_test<int32_t, std::string>(pool, "mt_s_str", order, elements_count);
+        success &= run_multithreading_test<int32_t, std::wstring>(pool, "mt_s_wstr", order, elements_count);
+        success &= run_multithreading_test<int32_t, const char*>(pool, "mt_s_blob", order, elements_count);
         BOOST_REQUIRE_MESSAGE(success, "TEST_MULTITHREADING");
     }
 
